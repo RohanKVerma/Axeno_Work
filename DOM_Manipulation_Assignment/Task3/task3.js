c23b90f2e190e24891a03e06a08ae95edf761fee
Rohan Kumar Verma <rohan.verma@axeno.co>
2024-08-07 13:56:07 +0530

    Task 3 done

--- DOM_Manipulation_Assignment/Task3/task3.js
+++ DOM_Manipulation_Assignment/Task3/task3.js
@@ -2,52 +2,51 @@
 const InputListconfig = [
     {
       type: "text",
-      id: "",
+      id: "name",
       name: "full_name",
-      maxLength: "",
-      minLength: "",
+      maxLength: "30",
+      minLength: "3",
       pattern: "",
-      placeholder: "",
-      required: "",
+      placeholder: "Enter your full name",
+      required: "yes",
       value: "",
-      label: "",
+      label: "Name",
     },
     {
       type: "email",
-      id: "",
+      id: "mail_id",
       name: "email",
-      maxLength: "",
-      minLength: "",
-      pattern: "",
-      placeholder: "",
-      required: "",
+      minLength: "5",
+      pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$",
+      placeholder: "Enter your mail id",
+      required: "yes",
       value: "",
-      label: "",
+      label: "Email",
     },
-    { type: "checkbox", id: "", name: "accept", required: "", checked: "", value: "", label: "" },
+    { type: "checkbox", id: "confirmation", name: "accept", required: "", checked: "", value: "", label: "I accept all the terms and conditions." },
     {
       type: "password",
-      id: "",
+      id: "pass",
       name: "password",
       maxLength: "",
-      minLength: "",
-      pattern: "",
-      placeholder: "",
-      required: "",
+      minLength: "6",
+      pattern: "[a-z0-9._%@$]",
+      placeholder: "Create password",
+      required: "yes",
       value: "",
-      label: "",
+      label: "Create password",
     },
-    { type: "date", id: "", name: "dob", max: "", min: "", placeholder: "", required: "", value: "", label: "" },
-    { type: "number", id: "", name: "marks", min: "", max: "", placeholder: "", required: "", value: "", label: "" },
-    { type: "radio", id: "", name: "gender", required: "", checked: "", value: "Male", label: "" },
-    { type: "radio", id: "", name: "gender", required: "", checked: "", value: "Female", label: "" },
+    { type: "date", id: "dob", name: "dob", max: "", min: "", placeholder: "", required: "", value: "", label: "Enter your Date of Birth" },
+    { type: "number", id: "phone", name: "marks", min: "", max: "", placeholder: "", required: "", value: "", label: "Enter your phone number" },
+    { type: "radio", id: "male", name: "gender", required: "", checked: "", value: "Male", label: "Male" },
+    { type: "radio", id: "female", name: "gender", required: "", checked: "", value: "Female", label: "Female" },
     {
       type: "select",
-      id: "",
+      id: "place",
       name: "state",
-      required: "",
+      required: "yes",
       value: "delhi",
-      label: "",
+      label: "Select your place",
       options: [
         { label: "Delhi", value: "delhi" },
         { label: "Mumbai", value: "mumbai" },
@@ -56,8 +55,66 @@ const InputListconfig = [
     },
   ];
 
-  const form = document.querySelector(".generated-form");
-  const formElement = form.createElement('input');
-  InputListconfig.forEach(formElement => {
-    formElement.setAttribute(InputListconfig);
-  });
+  // const form = document.querySelector("#generated-form");
+  // const formElement = form.createElement('input');
+  // InputListconfig.forEach(formElement => {
+  //   formElement.setAttribute(InputListconfig);
+  // });
+  const form = document.querySelector('#generated-form');
+  const button = document.querySelector('.btn');
+  const submit = document.querySelector('.submit_btn');
+
+
+  function generate(){
+    InputListconfig.forEach(field => {
+      button.classList.replace("visible","invisible");
+      submit.classList.replace("invisible","visible")
+      createLabel(field);
+      if(field.type == 'select'){
+        createSelect(field);
+      }else{
+        createInput(field);
+      }
+    });
+  }
+
+  function createLabel(field) {
+    if(!field.label == ""){
+      const labelElement = document.createElement("label");
+      labelElement.setAttribute("for", field.id);
+      labelElement.textContent = field.label;
+      form.appendChild(labelElement);
+    }
+  }
+
+  function createInput(field){
+    const input = document.createElement('input');
+
+    input.setAttribute("type",field.type);
+    input.setAttribute("name",field.name);
+    input.setAttribute("id",field.id);
+    // if(field.maxLength.length > 0){
+    //   input.setAttribute("maxlength",field.maxLength);
+    // }
+    // if(field.minLength.length > 0){
+    //   input.setAttribute("minlength",field.minLength);
+    // }
+    // if(field.pattern.length){
+    //   input.setAttribute("pattern",field.pattern);
+    // }
+    form.appendChild(input);
+  }
+
+  function createSelect(field){
+    const select = document.createElement('select');
+    select.setAttribute("name",field.name);
+    // option.textContent = 
+    // console.log(field.options[0].label);
+    field.options.forEach(label => {
+    const option = document.createElement('option');
+    option.textContent = label.label;
+    select.appendChild(option);
+
+    });
+    form.appendChild(select);
+  }
