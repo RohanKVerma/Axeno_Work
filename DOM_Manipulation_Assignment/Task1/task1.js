9b5b10e4188d79d225cebab84b5176f0398fbf59
Rohan Kumar Verma <rohan.verma@axeno.co>
2024-08-07 15:14:15 +0530

    New feature: Added error for adding empty or duplicate tags

--- DOM_Manipulation_Assignment/Task1/task1.js
+++ DOM_Manipulation_Assignment/Task1/task1.js
@@ -1,15 +1,25 @@
 const ul = document.querySelector("ul");
 let tags=[];
 
+const error = document.querySelector(".error_popup");
+
 function addTag(){
     let tag = document.getElementById("inputtext").value;
-    document.getElementById("inputtext").value = "";
-    ul.innerHTML='';
+    document.getElementById("inputtext").value = ""; 
         if(tag.length > 1 && !tags.includes(tag)){
             tag.split(',').forEach(tag =>{
-                tags.push(tag);
+                tag=tag.trim();
+                if(tag.length > 1){
+                    tags.push(tag);
+                }else{
+                     error.classList.replace("invisible","visible");
+                }
             });
+        }else{
+            console.log("Tag length is empty or already same tag available.");
+            error.classList.replace("invisible","visible");
         }
+        ul.innerHTML='<input type="text" name="" id="inputtext">';
         tags.forEach(tag=>{
             let liTag = `<li class="tagItem ${tag}">${tag}<div onclick="removeTag(this, '${tag}')"><img class="cross_icon ${tag}-tag" src="cross.svg" alt=""></div></li>`;
             ul.insertAdjacentHTML("afterbegin",liTag);
@@ -23,3 +33,13 @@ function removeTag(element, tag){
     element.parentElement.remove();
 
 }
+function checkError(){
+    let inputText = document.getElementById("inputtext").value;
+    if(error.classList.contains("visible")){
+        if(inputText.length > 0){
+            error.classList.replace("visible","invisible");
+        }
+    }
+}
+setInterval(checkError,100);
+checkError();
