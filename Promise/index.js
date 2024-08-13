cfbc4e1d7cbd48bb07a99d5b4e04e9f7bd6ace1a
Rohan Kumar Verma <rohan.verma@axeno.co>
2024-08-13 12:13:44 +0530

    Added filter and search

--- Promise/index.js
+++ Promise/index.js
@@ -1,24 +1,18 @@
+let ramList = [];
 var fetchedData = [];
-
 let obj;
-
-// async function fetchData() {
-//     return fetch('https://webscraper.io/test-sites/e-commerce/allinone/computers/tablets')
-// }
-
 fetch('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops')
   .then(res => res.text())
   .then(data => {
     obj = data;
    })
   .then(() => {
-    console.log(obj);
     const parser = new DOMParser();
     const doc = parser.parseFromString(obj, 'text/html');
-    console.log(doc);
     
     const cardDetail = doc.querySelectorAll('.description');
-    const imgURL = "https://webscraper.io/images/test-sites/e-commerce/items/cart2.png";
+    const imgcontainer = doc.querySelector(".img-fluid").getAttribute("src");
+    const imgURL = `https://webscraper.io${imgcontainer}`;
     
     cardDetail.forEach((desc) => {
         const prodDesc = desc.textContent;
@@ -26,14 +20,36 @@ fetch('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops')
         const prodTitle = desc.previousElementSibling.textContent;
         const prodPrice = desc.previousElementSibling.previousElementSibling.textContent;
 
+        const procMatch = prodDesc.match(/\b(?:Intel\s|AMD\s)?\w+\s\w+\b/);
+        const proc = procMatch ? procMatch[0] : "Unknown";
+        const ram = prodDesc.match(/\b\d+GB\b/);
+        const ramValue = ram ? ram[0] : "Unknown";
+
         fetchedData.push({
             "productDesc": prodDesc,
             "productImage": prodImg,
             "productTitle": prodTitle,
-            "productPrice": prodPrice
+            "productPrice": prodPrice,
+            "productProcessor": proc,
+            "productRAM": ramValue
         });
-        console.log(fetchedData);
     });
+    
+    ramList = [...new Set(fetchedData.map(item => item.productRAM))];
+    const ramBar = document.getElementById('categories');
+    ramList.sort();
+    console.log(ramList);   
+    ramList.map((ArrayData)=>{
+        const categoryItem = document.createElement('div');
+        categoryItem.classList.add('category');
+        categoryItem.classList.add(`${ArrayData}`);
+        const btn = document.createElement('button');
+        btn.setAttribute('id',ArrayData);
+        btn.innerText = ArrayData;
+        categoryItem.appendChild(btn);
+        ramBar.appendChild(categoryItem);
+    })
+    
     
     fetchedData.forEach(item => {
         const container = document.createElement("div");
@@ -41,11 +57,45 @@ fetch('https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops')
         container.innerHTML = 
         `<div class="card">
         <img src="${item.productImage}" alt="${item.productTitle}">
-        <h2>${item.productTitle}</h2>
+        <h2 class="productTitle">${item.productTitle}</h2>
         <div class="description">${item.productDesc}</div>
         <div class="itemPrice">Price: $${item.productPrice}</div>
+        <div class="itemProcessor">Processor:${item.productProcessor}</div>
+        <div class="itemRAM">RAM: ${item.productRAM}</div>
         <div>`;        
         document.querySelector('#cardContainer').appendChild(container);
     });
 })
 .catch(console.error);
+
+
+//Filter item with search text
+const searchBox = document.querySelector('#searchbar');
+searchBox.addEventListener('keyup', function(){
+    const searchValue = searchBox.value.toLowerCase();
+    const items = document.querySelectorAll('.card-text');
+    items.forEach(function(item){
+        const itemProcessor = item.querySelector('.itemProcessor').textContent.toLowerCase();
+        const itemRAM = item.querySelector('.itemRAM').textContent.toLowerCase();
+        if(itemProcessor.includes(searchValue) || itemRAM.includes(searchValue)){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    });
+});
+
+//Filter item with category button
+const parent = document.getElementById('categories');
+parent.addEventListener('click', (e)=>{
+    const filter = e.target.id.toLowerCase();
+    const items = document.querySelectorAll('.card-text');
+    items.forEach(function(item){
+        const itemCategory = item.querySelector('.itemRAM').textContent.toLowerCase();    
+        if(itemCategory.includes(filter) || filter == 'all'){
+            item.style.display = 'block';
+        }else{
+            item.style.display = 'none';
+        }
+    });
+})
